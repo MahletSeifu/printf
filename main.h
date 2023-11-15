4e1e5e9cd4169ee70eaf687ed8053f4cd3c6a696
Mahlet Seifu <akotet2123@gmail.com>
2023-11-15 12:47:10 +0300

    main print function

---
+++ main.h
@@ -0,0 +1,48 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define BUFF_SIZE 1024
+
+char *char_str(va_list ap);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *s);
+char *rev_str(char *str, int num);
+char *str_str(va_list ap);
+char *print_num(int n, int mul, int len);
+char *num_str(va_list ap);
+char *binary(va_list ap);
+char *dec_octal(va_list ap);
+char *int_hex(va_list ap);
+char *int_heX(va_list ap);
+char *oct_str(int num, int x, int mul, int len);
+char * _strrev(char *dest, char *s);
+char *_unsigned(va_list ap);
+char *unsigned_str(int num, int x, int mul, int len);
+char *str_rev(va_list ap);
+char *_strrot13(char *dest, char *str);
+char *str_rot13(va_list ap); 
+char *create_buf(void);
+char* (*get_function(char c))(va_list ap);
+void print_buffer(char *buffer, int len, va_list ap);
+int _printf(const char *format, ...);
+
+
+/**
+ * struct data - contains two elements
+ * @c: character
+ * @fun: pointer to function
+ *
+ * Description: contains a character and pointer to function
+ * for passing it as an argument.
+ */
+
+typedef struct data
+{
+	char c;
+	char* (*fun)(va_list);
+} data_t;
+
+#endif
